b87676e97fc1a0fe2d80576c3bb5d12e949371a2
bas <bastien.billey@gmail.com>
2024-01-12 17:47:24 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,47 @@
-"use client";
-
+import { headers } from "next/headers";
 import { Suspense, useEffect, useState } from "react";
 import { appBaseUrl } from "./lib/utils";
-import { IpifyResult } from "./lib/ipify";
+import { IpifyResult, searchQuery } from "./lib/ipify";
 
-// function IP() {
-//   const FALLBACK_IP_ADDRESS = "0.0.0.0";
-//   const forwardedFor = headers().get("x-forwarded-for");
+function IP() {
+  const FALLBACK_IP_ADDRESS = "0.0.0.0";
+  const forwardedFor = headers().get("x-forwarded-for");
 
-//   if (forwardedFor) {
-//     return forwardedFor.split(",")[0] ?? FALLBACK_IP_ADDRESS;
-//   }
-
-//   return headers().get("x-real-ip") ?? FALLBACK_IP_ADDRESS;
-// }
-
-async function getData() {
-  const res = await fetch(`${appBaseUrl()}/api/location`);
-  if (!res.ok) {
-    return undefined;
+  if (forwardedFor) {
+    return forwardedFor.split(",")[0] ?? FALLBACK_IP_ADDRESS;
   }
 
-  const data = (await res.json()) as IpifyResult;
-
-  return data;
+  return headers().get("x-real-ip") ?? FALLBACK_IP_ADDRESS;
 }
 
-export default function Home() {
-  const [data, setData] = useState<undefined | IpifyResult>();
+async function getData(ip: string) {
+  return searchQuery(ip)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(
+          `Network response was not ok (${res.status} ${res.statusText})`
+        );
+      }
+      return res.json();
+    })
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      return undefined;
+    });
+}
 
-  useEffect(() => {
-    getData()
-      .then((res) => setData(res))
-      .catch((err) => console.error(err));
-  }, []);
+export default async function Home() {
+  const data = await getData(IP());
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {data && <p>{data.ip}</p>}
+
+      <Suspense fallback={null}>
+        <IP />
+      </Suspense>
     </main>
   );
 }
