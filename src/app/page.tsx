5e6341fe534a9f276fc4f49b906d7a4e3f51f6b2
bas <bastien.billey@gmail.com>
2024-01-12 17:02:38 +0100

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ async function getData() {
 }
 
 export default async function Home() {
-  const ip = await getData();
+  // const ip = await getData();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
